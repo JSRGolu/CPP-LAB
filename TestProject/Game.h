c60b43a431ed9882ef0108bdbd5bc7c63f27c3b3
JSRGolu <lokeshvashisht4321@gmail.com>
2025-01-25 18:39:15 +0000

    completed Game.h

--- TestProject/Game.h
+++ TestProject/Game.h
@@ -7,34 +7,36 @@ class Game
 public:
 
 	// Declare a default constructor and a default destructor.
-
+	Game();
+	~Game();
 
 	// Declare a function for handling the input of the user. Returns void and receives no parameters.
-
+	void handleInputs();
 
 	// Declare a function for updating the game state. Returns void and receives no parameters.
-
+	void update();
 
 	// Declare a function for rendering the game to the screen. Returns void and receives no parameters.
-
+	void render();
 
 	// Declare a function for returning a pointer to the "window" member of this class. Receives no parameters.
-
+	Window* getWindow();
 
 private:
 
 	// Declare a function for the logic of this demo: moving and bouncing the mushroom against the window limits.
 	// This fucntions returns void and receives no parameters.
-
+	void movement();
 
 	// Declare a member variable of type Window for the window of the application. Note this is NOT a pointer to a Window object.
-
+	Window windowApp();
 
 	// Declare a member variable of type Vector2f, that will indicate the movement of the mushroom per frame. Again, not a pointer.
-
+	Vector2f movementPF();
 
 	// Declare two member variables of types sf::Texture and sf::Sprite. The first one will hold the texture of the mushroom, while the
 	// second will be the sprite to draw in the window. These are variables, not pointers.
-
+	sf::Texture mushroomTexture;
+	sf::Sprite mushroomSprite;
 };
 
